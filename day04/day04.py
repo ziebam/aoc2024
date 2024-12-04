000752eebae6f16aaa297858252ff7aeb01625c8
ziebam <ziebamichal@tutanota.com>
2024-12-04 13:46:36 +0100

    Add solution for day 4

---
+++ day04/day04.py
@@ -0,0 +1,112 @@
+# TODO: For part 1, rather than brute-forcing, get matrices rotated by -45, 45, and 90 degrees,
+# and do a simple substring search in them ("XMAS", "SAMX").
+
+from performance_utils.performance_utils import measure_performance
+
+with open("day04/in04.txt") as in04:
+    data = [row.strip() for row in in04.readlines()]
+
+
+def part1(data):
+    out = 0
+
+    width = len(data[0])
+    height = len(data)
+    for y, row in enumerate(data):
+        for x, letter in enumerate(row):
+            if letter != "X":
+                continue
+
+            if y > 2:  # up
+                if (
+                    data[y - 1][x] == "M"
+                    and data[y - 2][x] == "A"
+                    and data[y - 3][x] == "S"
+                ):
+                    out += 1
+
+            if y < height - 3:  # down
+                if (
+                    data[y + 1][x] == "M"
+                    and data[y + 2][x] == "A"
+                    and data[y + 3][x] == "S"
+                ):
+                    out += 1
+
+            if x > 2:  # left
+                if (
+                    data[y][x - 1] == "M"
+                    and data[y][x - 2] == "A"
+                    and data[y][x - 3] == "S"
+                ):
+                    out += 1
+
+            if x < width - 3:  # right
+                if (
+                    data[y][x + 1] == "M"
+                    and data[y][x + 2] == "A"
+                    and data[y][x + 3] == "S"
+                ):
+                    out += 1
+
+            if x > 2 and y > 2:  # up-left
+                if (
+                    data[y - 1][x - 1] == "M"
+                    and data[y - 2][x - 2] == "A"
+                    and data[y - 3][x - 3] == "S"
+                ):
+                    out += 1
+
+            if x < width - 3 and y < height - 3:  # down-right
+                if (
+                    data[y + 1][x + 1] == "M"
+                    and data[y + 2][x + 2] == "A"
+                    and data[y + 3][x + 3] == "S"
+                ):
+                    out += 1
+
+            if x < width - 3 and y > 2:  # up-right
+                if (
+                    data[y - 1][x + 1] == "M"
+                    and data[y - 2][x + 2] == "A"
+                    and data[y - 3][x + 3] == "S"
+                ):
+                    out += 1
+
+            if x > 2 and y < height - 3:  # down-left
+                if (
+                    data[y + 1][x - 1] == "M"
+                    and data[y + 2][x - 2] == "A"
+                    and data[y + 3][x - 3] == "S"
+                ):
+                    out += 1
+    return out
+
+
+def part2(data):
+    out = 0
+
+    width = len(data[0])
+    height = len(data)
+    for y, row in enumerate(data):
+        if y == 0 or y == height - 1:
+            continue
+
+        for x, letter in enumerate(row):
+            if x == 0 or x == width - 1:
+                continue
+
+            if letter != "A":
+                continue
+
+            uldr_diagonal = data[y - 1][x - 1] + "A" + data[y + 1][x + 1]
+            dlur_diagonal = data[y + 1][x - 1] + "A" + data[y - 1][x + 1]
+
+            if uldr_diagonal in ["MAS", "SAM"] and dlur_diagonal in ["MAS", "SAM"]:
+                out += 1
+
+    return out
+
+
+measure_performance("part 1", part1, data)
+measure_performance("part 2", part2, data)
