77f104f026198648508a56a8defeb21303cd7f0e
ziebam <ziebamichal@tutanota.com>
2024-12-11 20:21:48 +0100

    Improve solution for day 11 and solve both parts

--- day11/day11.py
+++ day11/day11.py
@@ -1,29 +1,40 @@
-# TODO: Part 2. It's probably something to do with individual digits as every number eventually
-# converges to one or more of them (Collatz conjecture at home), and they have repeatable results
-# after each iteration.
+from performance_utils.performance_utils import measure_performance
 
 with open("day11/in11.txt") as in11:
     data = in11.read().strip()
 
 
-def part1(data):
-    nums = [int(num) for num in data.split()]
+def both_parts(data, is_part_one):
+    counts = {}
+    for num in data.split():
+        num = int(num)
+        counts[num] = counts.get(num, 0) + 1
 
-    for _ in range(25):
-        new = []
-        for num in nums:
-            if num == 0:
-                new.append(1)
-            elif len(str(num)) % 2 == 1:
-                new.append(num * 2024)
+    n = 25 if is_part_one else 75
+    for _ in range(n):
+        next_counts = {}
+        for key, value in counts.items():
+            if key == 0:
+                next_counts[1] = next_counts.get(1, 0) + value
+            elif len(str(key)) % 2 == 1:
+                n = key * 2024
+                next_counts[n] = next_counts.get(n, 0) + value
             else:
-                snum = str(num)
-                new.append(int(snum[: len(snum) // 2]))
-                new.append(int(snum[len(snum) // 2 :]))
+                s = str(key)
+                s1 = int(s[: len(s) // 2])
+                s2 = int(s[len(s) // 2 :])
+                next_counts[s1] = next_counts.get(s1, 0) + value
+                next_counts[s2] = next_counts.get(s2, 0) + value
+        counts = next_counts
 
-        nums = new
+    out = 0
+    for value in counts.values():
+        out += value
 
-    return len(nums)
+    return out
 
 
-print(part1(data))
+measure_performance("part 1", both_parts, data, True)
+measure_performance(
+    "part 2", both_parts, data, False, warmup_runs=100, actual_runs=1000
+)
