c734214150bdec1eeb097feb9d5d29dacf5f5bf6
ziebam <ziebamichal@tutanota.com>
2024-12-12 15:04:15 +0100

    Clean up day 12 part 1

--- day12/day12.py
+++ day12/day12.py
@@ -1,31 +1,36 @@
+# TODO: Better approach for part 2.
+
 from performance_utils.performance_utils import measure_performance
 
 with open("day12/in12.txt") as in12:
     data = [row.strip() for row in in12.readlines()]
 
 
-def get_perimeter(grid, width, height, plant, region):
-    n = 0
-
-    for node in region:
-        x, y = node
-
-        if x == width - 1 or grid[y][x + 1] != plant:
-            n += 1
-
-        if y == height - 1 or grid[y + 1][x] != plant:
-            n += 1
-
-        if x == 0 or grid[y][x - 1] != plant:
-            n += 1
-
-        if y == 0 or grid[y - 1][x] != plant:
-            n += 1
-
-    return n
+width = len(data[0])
+height = len(data)
 
 
-def get_fences(grid, width, height, plant, region):
+# `region` is populated as a side effect. The returned value is said region's perimeter.
+def flood_fill(grid, plant, x, y, region):
+    if (x, y) in region:
+        return 0
+
+    if grid[y][x] != plant:
+        return 1
+
+    region.add((x, y))
+
+    perimeter = 0
+
+    perimeter += flood_fill(grid, plant, x + 1, y, region) if x < width - 1 else 1
+    perimeter += flood_fill(grid, plant, x, y + 1, region) if y < height - 1 else 1
+    perimeter += flood_fill(grid, plant, x - 1, y, region) if x > 0 else 1
+    perimeter += flood_fill(grid, plant, x, y - 1, region) if y > 0 else 1
+
+    return perimeter
+
+
+def get_fences(grid, plant, region):
     fences = set()
 
     for node in region:
@@ -46,33 +51,8 @@ def get_fences(grid, width, height, plant, region):
     return fences
 
 
-def flood_fill(grid, width, height, plant, x, y, region):
-    if (x, y) in region:
-        return
-
-    if grid[y][x] != plant:
-        return
-
-    region.add((x, y))
-
-    if x < width - 1:
-        flood_fill(grid, width, height, plant, x + 1, y, region)
-
-    if y < height - 1:
-        flood_fill(grid, width, height, plant, x, y + 1, region)
-
-    if x > 0:
-        flood_fill(grid, width, height, plant, x - 1, y, region)
-
-    if y > 0:
-        flood_fill(grid, width, height, plant, x, y - 1, region)
-
-
 def part1(data):
     checked = set()
-
-    width = len(data[0])
-    height = len(data)
 
     out = 0
     for y, row in enumerate(data):
@@ -81,11 +61,9 @@ def part1(data):
                 continue
 
             region = set()
-            flood_fill(data, width, height, plant, x, y, region)
+            perimeter = flood_fill(data, plant, x, y, region)
             checked.update(region)
-
             area = len(region)
-            perimeter = get_perimeter(data, width, height, plant, region)
 
             out += area * perimeter
 
@@ -95,9 +73,6 @@ def part1(data):
 def part2(data):
     checked = set()
 
-    width = len(data[0])
-    height = len(data)
-
     out = 0
     for y, row in enumerate(data):
         for x, plant in enumerate(row):
@@ -105,11 +80,11 @@ def part2(data):
                 continue
 
             region = set()
-            flood_fill(data, width, height, plant, x, y, region)
+            flood_fill(data, plant, x, y, region)
             checked.update(region)
-
             area = len(region)
-            fences = get_fences(data, width, height, plant, region)
+
+            fences = get_fences(data, plant, region)
 
             unique_fences = 0
 
@@ -154,6 +129,8 @@ def part2(data):
     return out
 
 
+# Probably off by a few microseconds since `width` and `height` are globals for convenience,
+# and therefore not calculated into the runtime, but I think it's negligible.
 measure_performance("part 1", part1, data)
 print(
     f"Part 2 answer: \033[92m{part2(data)}\x1b[0m. Too slow for performance measurement.\n"
