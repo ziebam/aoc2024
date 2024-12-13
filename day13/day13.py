486290307c328e6527872b9f9a99b20b4a2c7dc1
ziebam <ziebamichal@tutanota.com>
2024-12-13 14:46:07 +0100

    Add an explanation to the day 13 solution

--- day13/day13.py
+++ day13/day13.py
@@ -21,15 +21,25 @@ def both_parts(data, is_part_one):
         prize_x = int(prize[1].split(",")[0]) + (
             0 if is_part_one else 10_000_000_000_000
         )
+
         prize_y = int(prize[2]) + (0 if is_part_one else 10_000_000_000_000)
+
+        # Interpret each machine's parameters as two line equations in the standard form.
+        # Find their intersection (the input is nice in that the lines are never collinear).
+        # If any of the intersection's coordinates is not an integer, that machine is not possible
+        # to win on as you can't push a button a non-integer amount of times.
+
+        # a1 = ax, b1 = bx, a2 = ay, b2 = by, c1 = -prize_x, c2 = -prize_y
+        # x = (b1 * c2 - b2 * c1) / (a1 * b2 - a2 * b1)
+        # y = (a2 * c1 - a1 * c2) / (a1 * b2 - a2 * b1)
 
         denominator = ax * by - ay * bx
 
-        x = abs((bx * prize_y - by * prize_x) / denominator)
+        x = (bx * -prize_y - by * -prize_x) / denominator
         if int(x) != x:
             continue
 
-        y = abs((prize_x * ay - prize_y * ax) / denominator)
+        y = (-prize_x * ay - -prize_y * ax) / denominator
         if int(y) != y:
             continue
 
